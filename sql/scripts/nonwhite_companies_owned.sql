1608f7691c08397a936de1fc50bfc7a26bbb623b
Grzegorz Sochaj <ess.grzegorz.sochaj@gmail.com>
2020-11-06 11:58:00 +0100

    Added nonwhite_ownership script and chart
    Added script which decribes data about percent of nonwhite companies owned for county per winning party in that county. Also added useful chart.

---
+++ sql/scripts/nonwhite_companies_owned.sql
@@ -0,0 +1,13 @@
+select party,
+	avg(nonwhite_ownership) as AVG,
+	percentile_disc(0.25) within group (order by nonwhite_ownership) as Q1,
+	mode() within group (order by nonwhite_ownership) as MEDIAN,
+	percentile_disc(0.75) within group (order by nonwhite_ownership) as Q3,
+	stddev(nonwhite_ownership) as STD
+from (
+	select county, party, candidate, votes ,SBO315207+SBO115207+SBO215207+SBO515207+SBO415207 as nonwhite_ownership
+	from (
+		select *
+		from winning_per_county wpc  
+		join county_facts_csv cfc on wpc.fips = cfc.fips) xxx) xxxx
+group by party 
