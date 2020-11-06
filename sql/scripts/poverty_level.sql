96c28d735021b540121997dcc0f2d9d96c33393f
Grzegorz Sochaj <ess.grzegorz.sochaj@gmail.com>
2020-11-06 13:39:17 +0100

    Minor changes to scripts and charts

--- sql/scripts/poverty_level.sql
+++ sql/scripts/poverty_level.sql
@@ -5,10 +5,19 @@ select party,
 	percentile_disc(0.75) within group (order by poverty_level) as Q3,
 	stddev(poverty_level) as STD
 from (
-	select 
-	distinct atd.fips, atd.county, wpc.party ,wpc.candidate , atd.pvy020213 as poverty_level
-	from all_together_dropped atd 
-	full join winning_per_county wpc on atd.fips = wpc.fips
-	where atd.fips is not null
-	order by atd.pvy020213 desc) xxxx
+	(select county, party, candidate, votes , pvy020213 as poverty_level
+			from (
+				select *
+				from winning_per_county wpc  
+				join county_facts_csv cfc on wpc.fips = cfc.fips) xxx
+				where party like('Democrat')
+				limit 939)
+		union (
+		select county, party, candidate, votes ,pvy020213 as poverty_level
+			from (
+				select *
+				from winning_per_county wpc  
+				join county_facts_csv cfc on wpc.fips = cfc.fips) xxx
+				where party like('Republican')
+				limit 939)) xxxx
 group by party 
--- sql/scripts/veterans script.sql
+++ sql/scripts/veterans script.sql
@@ -5,13 +5,19 @@ select party,
 	round(percentile_disc(0.75) within group (order by veterans_factor),2) as Q3,
 	round(stddev(veterans_factor),2) as STD
 from (
-	select 
-	distinct atd.fips, atd.county, wpc.party ,wpc.candidate , round(atd.vet605213::numeric / atd.pst045214::numeric,2) as veterans_factor
-	from all_together_dropped atd 
-	full join winning_per_county wpc on atd.fips = wpc.fips
-	where atd.fips is not null
-	order by veterans_factor desc) xxxx
+	(select county, party, candidate, votes , round(vet605213::numeric / pst045214::numeric,2) as veterans_factor
+			from (
+				select *
+				from winning_per_county wpc  
+				join county_facts_csv cfc on wpc.fips = cfc.fips) xxx
+				where party like('Democrat')
+				limit 939)
+		union (
+		select county, party, candidate, votes ,round(vet605213::numeric / pst045214::numeric,2) as veterans_factor
+			from (
+				select *
+				from winning_per_county wpc  
+				join county_facts_csv cfc on wpc.fips = cfc.fips) xxx
+				where party like('Republican')
+				limit 939)) xxxx
 group by party 
-	
-	
-	
