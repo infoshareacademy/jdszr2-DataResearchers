d9595e6b9ab2353539d66d2ac84ed2d8c807d6ab
pawlowski-sptech <71319049+pawlowski-sptech@users.noreply.github.com>
2020-11-15 09:31:04 +0100

    Added races and density scripts

---
+++ sql/scripts/view_races.sql
@@ -0,0 +1,42 @@
+create or replace view races as
+
+with
+facts_and_winners as (
+	select * from winning_per_county wpc
+	join county_facts_csv cfc on wpc.fips = cfc.fips
+),
+democrats as (
+  select
+		county, party, candidate, votes,
+		RHI825214 as white,
+  	RHI225214 as black,
+  	RHI325214+RHI525214 as indians,
+  	RHI725214 as latin,
+  	RHI425214 as asians
+  from facts_and_winners
+	where party like('Democrat')
+	order by random() limit 939
+),
+republicans as (
+	select
+		county, party, candidate, votes,
+		RHI825214 as white,
+		RHI225214 as black,
+		RHI325214+RHI525214 as indians,
+		RHI725214 as latin,
+		RHI425214 as asians
+  from facts_and_winners
+  where party like('Republican')
+  order by random() limit 939
+),
+race as (
+	select * from democrats
+	union (
+		select * from republicans
+  )
+)
+
+select
+	county, party, white, black, latin, indians, asians,
+  latin+indians+asians as "others"
+from race
