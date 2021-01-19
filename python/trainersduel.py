78c90ccfb870a460a211fb3ceeea436b4166f6ca
Michal Pawlowski <pawlowski@sptech.pl>
2021-01-19 14:13:37 +0100

    draft ligi part2

--- python/trainersduel.py
+++ python/trainersduel.py
@@ -1,14 +1,16 @@
 import fight
 import tvec
 import random
-import dbloader
 import numpy as np
 import pandas as pd
 
+def import_league(path):
+    return pd.read_csv(path, sep=';')
+
+def select_trainers(df):
+    pass
 
 # RUN AS __main__ TO PERFORM MODULE TESTS
 if __name__ == "__main__":
-    pokemon_df, trainers_df, trainers_cp_df = dbloader.read()
-    print('dbloader.read() tests:', type(pokemon_df), type(trainers_df), type(trainers_cp_df))
-
-    print(trainers_cp_dfd)
+    df = import_league("league_test.csv")
+    select_trainers(df)
